62e207ee8fe76941c69f04f4b17ddff37b315148
marouncz <marouncz@gmail.com>
2023-11-22 14:35:27 +0100

    set spi pin as output

--- Xylophone/lib/spi/spi.c
+++ Xylophone/lib/spi/spi.c
@@ -2,6 +2,7 @@
 
 void initSPI(void)
 {
+    DDRB |= (1<<2); // set latch pin as output
     DDRB |= ((1<<3) | (1<<5)); //set SCLK and MOSI pins as outputs
     SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPR1)|(1<<DORD); //enable SPI peripheral as master with fclk/128 clock and LSB first
 }
