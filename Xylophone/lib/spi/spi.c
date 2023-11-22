e37285e7198b2661ee92bd8b6c52acf9d219691a
marouncz <marouncz@gmail.com>
2023-11-22 14:12:08 +0100

    initSPI function

--- Xylophone/lib/spi/spi.c
+++ Xylophone/lib/spi/spi.c
@@ -2,7 +2,8 @@
 
 void initSPI(void)
 {
-
+    DDRB |= ((1<<3) | (1<<5)); //set SCLK and MOSI pins as outputs
+    SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPR1)|(1<<DORD); //enable SPI peripheral as master with fclk/128 clock and LSB first
 }
 
 void transmitSPI(volatile uint8_t *data)
