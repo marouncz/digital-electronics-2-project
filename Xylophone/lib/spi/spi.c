ca66219e3bafa18029b1c54402ffda80a15d95a5
marouncz <marouncz@gmail.com>
2023-11-28 20:14:13 +0100

    changed spi clock speed

--- Xylophone/lib/spi/spi.c
+++ Xylophone/lib/spi/spi.c
@@ -4,7 +4,8 @@ void initSPI(void)
 {
     DDRB |= (1<<2); // set latch pin as output
     DDRB |= ((1<<3) | (1<<5)); //set SCLK and MOSI pins as outputs
-    SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPR1)|(1<<DORD); //enable SPI peripheral as master with fclk/128 clock and LSB first
+    SPCR |= (1<<SPE)|(1<<MSTR)|(1<<DORD); //enable SPI peripheral as master with fclk/4 clock and LSB first
+
 }
 
 void transmitSPI(volatile uint8_t *data)
