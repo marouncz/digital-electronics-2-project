12766caf403da26d8ca1cd3506bcaf67204bf650
marouncz <marouncz@gmail.com>
2023-11-28 22:33:21 +0100

    ding dong routine

--- Xylophone/src/main.c
+++ Xylophone/src/main.c
@@ -18,45 +18,46 @@
 
 /* Defines -----------------------------------------------------------*/
 #ifndef F_CPU
-#define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
+#define F_CPU 16000000 // CPU frequency in Hz required for UART_BAUD_SELECT
 #endif
 
 /* Includes ----------------------------------------------------------*/
 // System library headers
-#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
-#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h> // Interrupts standard C library for AVR-GCC
+#include <avr/io.h>        // AVR device-specific IO definitions
 #include <stdio.h>
-#include <stdlib.h>  // C library. Needed for number conversions
+#include <stdlib.h> // C library. Needed for number conversions
 #include <string.h>
 // Project specific library headers
-#include "gpio.h"            // Custom library for GPIO pin setup
-#include "gui.h"             // GUI library for oled display
-#include "pin_definition.h"  // Pin definitions
+#include "gpio.h"           // Custom library for GPIO pin setup
+#include "gui.h"            // GUI library for oled display
+#include "pin_definition.h" // Pin definitions
 #include "spi.h"
-#include "timer.h"  // Timer library for AVR-GCC
+#include "timer.h" // Timer library for AVR-GCC
 #include "uart.h"
 
 #define DING_DUR 10
 
 uint8_t prevButtonState[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 uint8_t currButtonState[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-uint8_t dingTime[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 /* Function definitions ----------------------------------------------*/
-int main(void) {
+int main(void)
+{
   // Init GUI on oled display
   gui_init();
-  GPIO_setup_xylophone();  // Pin direction setup
+  GPIO_setup_xylophone(); // Pin direction setup
   SPI_init();
   GPIO_mode_input_pullup(&DDRD, 2);
   uart_init(UART_BAUD_SELECT(9600, F_CPU));
 
-  TIM1_OVF_33MS;
+  TIM1_OVF_4MS;
   TIM1_OVF_ENABLE;
   sei();
 
   // Main loop
-  for (;;) {
+  for (;;)
+  {
   }
 
   // Will never reach this
@@ -64,37 +65,31 @@ int main(void) {
 }
 
 /* Interrupt service routines ----------------------------------------*/
-ISR(TIMER1_OVF_vect) {
-  // static uint8_t tone = 0;
+ISR(TIMER1_OVF_vect)
+{
+  static uint8_t dingTime[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  static uint8_t regData = 0;
 
-  // static uint8_t data = 0;
-  // data = 1<<tone;
-
-  // GPIO_write_low(&PORTB, 2);
-  // transmitSPI(&data);
-  // GPIO_write_high(&PORTB, 2);
-
-  // tone++;
-  // if(tone > 7) tone = 0;
-  // uint8_t str[4];
-  // itoa(data, &str, 10);
-  // uart_puts(&str);
-
-  // uart_puts(".\r\n");
   GPIO_read_pins(&currButtonState);
 
-  for (uint8_t i = 0; i < sizeof(currButtonState); i++) {
-    if (currButtonState[i] != prevButtonState[i]) {
+  for (uint8_t i = 0; i < sizeof(dingTime); i++)
+  {
+    if (currButtonState[i] == 0 && prevButtonState[i] == 1)
+    {
       dingTime[i] = DING_DUR;
     }
-    prevButtonState[i] = currButtonState[i];
-  }
-
-  for (uint8_t i = 0; i < sizeof(dingTime); i++) {
-    if (dingTime[i] > 0) {
+    if (dingTime[i] > 0)
+    {
+      regData |= (1 << i);
       dingTime[i]--;
     }
   }
+  for (uint8_t i = 0; i < sizeof(currButtonState); i++)
+  {
+    prevButtonState[i] = currButtonState[i];
+  }
 
-  uart_putc(dingTime[1]+48);
+
+  SPI_shift(regData);
+  regData = 0;
 }
