406fb72b855cdd35eb1730f35bf5b8b6b5e3b906
marouncz <marouncz@gmail.com>
2023-12-06 15:41:45 +0100

    added third song stored to progmem

--- Xylophone/lib/songs/songs.h
+++ Xylophone/lib/songs/songs.h
@@ -10,7 +10,7 @@ const uint16_t flashSongsTimeStamp1[] PROGMEM = {
 const uint8_t flashSongsNote1[] PROGMEM = {
 0, 1, 2, 3, 0, 2, 4, 2, 0, 2, 4, 2, 5, 5, 0, 2, 4, 5, 5, 0, 2, 4, 3, 3, 3, 3, 2, 2, 1, 1, 0, 2, 4, 3, 3, 3, 3, 2, 2, 1, 1, 0, 7, 0
 };
-
+//ovcaci ctveraci
 const uint16_t flashSongsTimeStamp2[] PROGMEM = {
 240, 328, 418, 633, 727, 819, 1035, 1080, 1128, 1174, 1223, 1311, 1418, 1462, 1514, 1566, 1616, 1702, 1794, 1893, 2110, 2115, 2141, 0
 };
@@ -20,11 +20,11 @@ const uint8_t flashSongsNote2[] PROGMEM = {
 };
 
 const uint16_t flashSongsTimeStamp3[] PROGMEM = {
-100, 200, 300, 350, 0
+583, 652, 721, 862, 995, 1055, 1115, 1174, 1230, 1360, 1487, 1549, 1605, 1665, 1725, 1836, 1953, 2016, 2072, 2134, 2196, 2313, 2417, 2477, 0
 };
-
+//kohutik jaraby
 const uint8_t flashSongsNote3[] PROGMEM = {
-1, 2, 3, 4, 0
+0, 1, 2, 3, 3, 3, 3, 2, 1, 2, 2, 2, 2, 1, 0, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0
 };
 
 
